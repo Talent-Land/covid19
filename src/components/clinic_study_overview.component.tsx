b2f25d187391e61b36143813708faf26040bc1d4
Daniel Acosta <da_co@hotmail.es>
2022-07-21 16:11:06 -0500

    Map integration

--- src/components/clinic_study_overview.component.tsx
+++ src/components/clinic_study_overview.component.tsx
@@ -1,13 +1,34 @@
+import { GoogleMap } from "./map.component";
+
 export const ClincStudyOverview = () => {
   return (
-    <div className=" bg-wine-themePrimary rounded-md drop-shadow-2xl mr-2 my-2 p-2">
-      {/* RESEARCH TITLE */}
-      <div className="text-xl font-bold pb-2">THIS IS A TEST</div>
-      <hr></hr>
-      {/* RESEARCH CONTENT */}
-      <div className="pt-2">
-        THIS IS THE CONTENT OF THE TEST, IM JUST TRYING TO FILL THE CARD SO IT
-        LOOKS LIKE IT HAS SOME REAL CONTENT
+    <div className="flex flex-grow">
+      <div className="basis-8/12 flex flex-col bg-blue-themeTertiary/80 rounded-md drop-shadow-2xl m-2 p-2">
+        <div className="flex flex-row">
+          <div className="basis-3/4 flex flex-col">
+            <div className="flex flex-row">
+              <p className="">TITLE</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="">SCIENTIFIC TITLE</p>
+            </div>
+          </div>
+          <div className="basis-1/4 flex flex-col">
+            <div className="flex flex-row self-end">
+              <p className="">INCLUSION AGE</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="">DATE PUBLICATION</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="basis-2/4 flex flex-col">
+            <GoogleMap />
+          </div>
+          <div className="basis-2/4 flex flex-col"></div>
+        </div>
+        <div className="flex flex-row"></div>
       </div>
     </div>
   );
---
+++ src/components/map.component.tsx
@@ -0,0 +1,25 @@
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useRef, useState, useEffect } from "react";
+
+const center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
+
+const render = (status: Status) => {
+  return <h1>{status}</h1>;
+};
+
+export const GoogleMap = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map>();
+
+  useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, { center, zoom: 4 }));
+    }
+  }, [ref, map]);
+
+  return (
+    <Wrapper apiKey={"AIzaSyDl5Dzght-xb4Y9vieduMNP5IJVoTDVtZQ"} render={render}>
+      <div ref={ref} className="w-64 h-64"/>
+    </Wrapper>
+  );
+};
