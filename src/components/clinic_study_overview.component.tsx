a04dd7e71e9cbb3ece14a338e6f9b94107fbe191
Daniel Acosta <da_co@hotmail.es>
2022-07-21 10:46:29 -0500

    Router

---
+++ src/components/clinic_study_overview.component.tsx
@@ -0,0 +1,14 @@
+export const ClincStudyOverview = () => {
+  return (
+    <div className=" bg-white rounded-md drop-shadow-2xl mr-2 my-2 p-2">
+      {/* RESEARCH TITLE */}
+      <div className="text-xl font-bold pb-2">THIS IS A TEST</div>
+      <hr></hr>
+      {/* RESEARCH CONTENT */}
+      <div className="pt-2">
+        THIS IS THE CONTENT OF THE TEST, IM JUST TRYING TO FILL THE CARD SO IT
+        LOOKS LIKE IT HAS SOME REAL CONTENT
+      </div>
+    </div>
+  );
+};
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "../App.css";
 import MainContainer from "../containers/main.container";
-import ResearchContainer from "../containers/research.container";
-import StatisticContainer from "../containers/statistic.container";
 
 function App() {
   return (
-    <div className="flex w-screen h-screen">
-      <div className="w-8/12 mt-4">
-        <StatisticContainer />
-        <MainContainer />
-      </div>
-
-      <ResearchContainer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<MainContainer />} />
+      </Route>
+    </Routes>
   );
 }
+
+const Layout = () => {
+  return (
+    <div className="flex w-screen h-screen">
+      <Outlet />
+    </div>
+  );
+};
 
 export default App;
