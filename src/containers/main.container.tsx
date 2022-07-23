57f78229155bf93bb0816c033e82080403d22a3d
Daniel Acosta <da_co@hotmail.es>
2022-07-23 01:53:51 -0500

    .gitignore package-lock

--- src/containers/main.container.tsx
+++ src/containers/main.container.tsx
@@ -7,9 +7,6 @@ import ResearchInfoContainer from "./research_info.container";
 export const MainContainer = () => {
   return (
     <>
-      {/* <div className="flex  bg-blue-themeLighterAlt -z-10">
-        <MapGraphicContainer />
-      </div> */}
       <div className="flex flex-col bg-transparent">
         <div className="flex place-content-start place-items-center bg-blue-themePrimary w-full h-12">
           <p className="text-3xl text-whiteT ml-10">ResearchSaver.com</p>
@@ -17,21 +14,23 @@ export const MainContainer = () => {
         <div className="flex flex-row gap-4 overflow-y-auto">
           <div className="flex flex-col basis-8/12 m-4">
             <StatisticContainer />
-            {/* <ResearchInfoContainer/> */}
+
+            <div className="flex absolute bottom-0 m-2 ">
+              <a
+                href="https://www.youtube.com/watch?v=djV11Xbc914"
+                target={"_blank"}
+              >
+                <img
+                  src={Calderon}
+                  alt="TechCauldron"
+                  className="h-12 opacity-25 hover:opacity-100 cursor-pointer"
+                />
+              </a>
+            </div>
           </div>
 
           <div className="flex flex-col basis-4/12 mt-4 bg-transparent mr-10 overflow-y-auto">
             <ResearchContainer />
-          </div>
-
-          <div className="flex absolute h-full items-end ml-2 ">
-            <a href="https://www.youtube.com/watch?v=djV11Xbc914">
-              <img
-                src={Calderon}
-                alt="TechCauldron"
-                className="h-12 opacity-25 hover:opacity-100 cursor-pointer"
-              />
-            </a>
           </div>
         </div>
       </div>
